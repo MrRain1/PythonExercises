a8f9d517618d79030f7a68ad2ad9a540da52969a
MrRain1 <b.degliuberti@outlook.com>
2022-07-20 18:21:10 +0200

    First Exercise in python

---
+++ Hello_World/hello_world.py
@@ -0,0 +1,12 @@
+print("Hello World")
+print("What is your name")
+
+myName = input() # input function: waits until user inputs in the terminal
+print("It's good to meet you " + myName)
+print("The lenght of your name is " + str(len(myName)))
+print("What's your age?")
+
+myAge = input() # input() returns strings
+print("You will be: " + str(int(myAge) + 1) + " in a year")
+# int() converts a string to int value
+# float() converts to a float value
