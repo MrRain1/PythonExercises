2be07d7079e4511ecbd3631a3d33f829e6caeb82
MrRain1 <b.degliuberti@outlook.com>
2022-07-21 13:07:11 +0200

    Finished Band name gen exercise

---
+++ 100_Days_Challenge/Day1_DebugPrint/Day1_Band_Name_Generator/main.py
@@ -0,0 +1,15 @@
+petName=""
+cityName=""
+#1. Create a greeting for your program.
+print("Hello! Wellcome to the band name generator!")
+#2. Ask the user for the city that they grew up in.
+if cityName =="":
+    print("Could you tell me in what city did your grow up in?")
+    cityName = input()
+#3. Ask the user for the name of a pet.
+if petName =="":
+    print("Could you tell me the name of your pet?")
+    petName = input()
+
+#4. Combine the name of their city and pet and show them their band name.
+print("The name of your band is: The "+ cityName + " " + petName)
