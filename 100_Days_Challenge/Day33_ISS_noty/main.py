2d282a1789c11976d3519e55654709e9aa0966d3
MrRain1 <b.degliuberti@outlook.com>
2023-04-09 18:18:47 +0200

    Added "main" entrypoint

--- 100_Days_Challenge/Day33_ISS_noty/main.py
+++ 100_Days_Challenge/Day33_ISS_noty/main.py
@@ -24,25 +24,26 @@ def is_overhead(iss_latitude, iss_longitude):
     iss_in_range= min_lat<=iss_latitude<=max_lat and min_lng<=iss_longitude<=max_lng
     return iss_in_range
 
+if __name__=="__main__":
+    iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    iss_response.raise_for_status()
+    iss_obtained_data = iss_response.json()
+    
+    longitude = float(iss_obtained_data["iss_position"]["longitude"])
+    latitude = float(iss_obtained_data["iss_position"]["latitude"])
+    
+    sunrise_sunset_response = requests.get(url="https://api.sunrise-sunset.org/json", params= PARAMETERS)
+    sunrise_sunset_response.raise_for_status()
+    sunrise_sunset_data = sunrise_sunset_response.json()
+    
+    timings = {
+        "sunrise": int(sunrise_sunset_data["results"]["sunrise"].split("T")[1].split(":")[0]),
+        "sunset": int(sunrise_sunset_data["results"]["sunset"].split("T")[1].split(":")[0]),
+    }
+    current_hour = datetime.datetime.now().hour
+    
+    if (is_overhead(iss_latitude=latitude, iss_longitude=longitude)) and not (timings["sunrise"]<current_hour<timings["sunset"]):
+        print("You can observe the ISS cruising the night sky!")
+    else:
+        print("Sorry the ISS is not yet observable.")
 
-iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
-iss_response.raise_for_status()
-iss_obtained_data = iss_response.json()
-
-longitude = float(iss_obtained_data["iss_position"]["longitude"])
-latitude = float(iss_obtained_data["iss_position"]["latitude"])
-
-sunrise_sunset_response = requests.get(url="https://api.sunrise-sunset.org/json", params= PARAMETERS)
-sunrise_sunset_response.raise_for_status()
-sunrise_sunset_data = sunrise_sunset_response.json()
-
-timings = {
-    "sunrise": int(sunrise_sunset_data["results"]["sunrise"].split("T")[1].split(":")[0]),
-    "sunset": int(sunrise_sunset_data["results"]["sunset"].split("T")[1].split(":")[0]),
-}
-current_hour = datetime.datetime.now().hour
-
-if (is_overhead(iss_latitude=latitude, iss_longitude=longitude)) and not (timings["sunrise"]<current_hour<timings["sunset"]):
-    print("You can observe the ISS cruising the night sky!")
-else:
-    print("Sorry the ISS is not yet observable.")
